702caf1de75b27e9950c399ca63e0acddb22c5f0
Anindya Biswas <parabolicanindya@gmail.com>
2024-11-29 00:09:14 +0600

    PlayerList class updated

--- src/FileOperations.java
+++ src/FileOperations.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -5,22 +6,24 @@ import java.io.FileWriter;
 
 public class FileOperations {
 
-    private static final String INPUT_FILE_NAME = "players.txt";
+    private static final String INPUT_FILE_NAME = "resource/players.txt";
     private static final String OUTPUT_FILE_NAME = "out.txt";
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE_NAME));
-        while (true) {
-            String line = br.readLine();
-            if (line == null) break;
-            System.out.println(line);            
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE_NAME))) {
+            while (true) {
+                String line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                System.out.println(line);
+            }
         }
-        br.close();
 
         String text = "Hello World";
-        BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME));
-        bw.write(text);
-        bw.write(System.lineSeparator());
-        bw.close();
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_FILE_NAME))) {
+            bw.write(text);
+            bw.write(System.lineSeparator());
+        }
     }
 }
---
+++ temp/temp1.java
@@ -0,0 +1,7 @@
+public class temp1 {
+    public static void main(String[] args) {
+        String temp1 = "Hello";
+        String temp2 = "Hello";
+        System.out.println();
+    }
+}
